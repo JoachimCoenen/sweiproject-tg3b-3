e0d8a50893b7fd20e5b830e6ed33db3bc653d06f
JoachimCoenen <joachimcoenen@icloud.com>
2017-11-30 15:02:50 +0100

    in ActivityDetails dialog the Delete and Edit buttons now working

--- src/main/resources/static/js/activity.js
+++ src/main/resources/static/js/activity.js
@@ -1,6 +1,6 @@
 
 function loadActivities ($scope, $http){
-	$http({
+	return $http({
 		 method : 'GET',
 		 /*
 		 url: (window.location.hostname === 'localhost' ?
@@ -14,79 +14,93 @@ function loadActivities ($scope, $http){
 	});
 }
 
+function loadActivity (activityId, $scope, $http){
+	return $http({
+		 method : 'GET',
+		url: 'activity/' + $scope.activity.id
+		}).then(function (response) {
+			 $scope.activity = response.data;
+	});
+}
+
+
+var viewActivityDialogOptions = {
+	controller: 'ViewActivityCtrl',
+	templateUrl: './templates/activityDetails.html'
+};
+function viewActivity_bare(activity, $scope, $http, $dialog) {
+	var activityToView = activity;
+	return $dialog.dialog(angular.extend(viewActivityDialogOptions, {resolve: {activity: angular.copy(activityToView)}})).open().then(function (){
+		loadActivities($scope, $http);
+	});
+};
+
+var editDialogOptions = {
+	controller: 'EditActivityCtrl',
+	templateUrl: './templates/activityEdit.html',
+};
+function editActivity_bare(activity, $scope, $http, $dialog) {
+	var activityToEdit = activity;
+	return $dialog.dialog(angular.extend(editDialogOptions, {resolve: {activity: angular.copy(activityToEdit)}})).open().then(function (){
+		loadActivities($scope, $http);
+	});
+};
+
+function deleteActivity_bare(activity, $scope, $http, $dialog) {
+		var deleteRequest = {
+			method : 'DELETE',
+			url: 'activity/' + activity.id
+		};
+		
+		return $http(deleteRequest).then(function() {
+			loadActivities($scope, $http);
+		});
+		//todo handle error
+	};
+
+
 //function initiateApp() {
 	var app = angular.module('ActivityMeterApp', ['ui.bootstrap']);
 
-	app.controller('ActivityCtrl', function ($scope, $http, $dialog) {
-
+app.controller('ActivityCtrl', function ($scope, $http, $dialog) {
 	loadActivities($scope, $http);
-
-		  
-	var viewActivityDialogOptions = {
-		controller: 'ViewActivityCtrl',
-		templateUrl: './templates/activityDetails.html'
-	};
-
+	
 	$scope.viewActivity = function(activity){
-		var activityToView = activity;
-		$dialog.dialog(angular.extend(viewActivityDialogOptions, {resolve: {activity: angular.copy(activityToView)}})).open();
+		viewActivity_bare(activity, $scope, $http, $dialog);
 	};
-
-		  
+	
 	var addDialogOptions = {
 		controller: 'AddActivityCtrl',
 		templateUrl: './templates/activityAdd.html'
 	};
-
 	$scope.add = function(activity){
 		$dialog.dialog(angular.extend(addDialogOptions, {})).open().then(function (){
 			loadActivities($scope, $http);
 		}) ;
 	};
 
-	var editDialogOptions = {
-		controller: 'EditActivityCtrl',
-		templateUrl: './templates/activityEdit.html',
-	};
 	$scope.edit = function(activity){
-		var activityToEdit = activity;
-		$dialog.dialog(angular.extend(editDialogOptions, {resolve: {activity: angular.copy(activityToEdit)}})).open().then(function (){
-			loadActivities($scope, $http);
-		}) ;
+		editActivity_bare(activity, $scope, $http, $dialog);
 	};
 		
 	$scope.delete = function(activity) {
-		var deleteRequest = {
-			method : 'DELETE',
-			url: 'activity/' + activity.id
-		};
-		
-		$http(deleteRequest).then(function() {
-			loadActivities($scope, $http);
-		});
-		//todo handle error
+		deleteActivity_bare(activity, $scope, $http, $dialog);
 	};
-	});
+});
 
-	app.controller('ViewActivityCtrl', function ($scope, $http, activity, dialog) {
-
+app.controller('ViewActivityCtrl', function ($scope, $http, activity, $dialog, dialog) {
 	$scope.activity = activity;
-	$scope.save = function($activity) {
-		var putRequest = {
-		method : 'PUT',
-		url: 'activity/' + $scope.activity.id,
-		data: {
-				text: $scope.activity.text,
-				tags: $scope.activity.tags,
-				title: $scope.activity.title
-			  }
-		}  
+	
+	$scope.edit = function(activity){
+		editActivity_bare(activity, $scope, $http, $dialog).then(function () {
+			loadActivity (activity.id, $scope, $http);
+		});
 		
-		$http(putRequest).then(function (response) {
-			$scope.activities = response.data;
-		}).then(function () {
-			//todo handle error
-			$scope.close();
+	};
+	
+	$scope.delete = function(activity) {
+		deleteActivity_bare(activity, $scope, $http, $dialog).then(function () {
+		dialog.close();
 		});
 	};
 
@@ -94,10 +108,9 @@ function loadActivities ($scope, $http){
 		loadActivities($scope, $http);
 		dialog.close();
 	};
-	});
+});
 	
-	app.controller('AddActivityCtrl', function($scope, $http, dialog){
-
+app.controller('AddActivityCtrl', function($scope, $http, dialog){
 	$scope.save = function(Activity) {
 		var postRequest = {
 		method : 'POST',
@@ -119,10 +132,9 @@ function loadActivities ($scope, $http){
 	$scope.close = function(){;
 		dialog.close(undefined);
 	};
-	});
+});
 
-	app.controller('EditActivityCtrl', function ($scope, $http, activity, dialog) {
-
+app.controller('EditActivityCtrl', function ($scope, $http, activity, dialog) {
 	$scope.activity = activity;
 	$scope.save = function($activity) {
 		var putRequest = {
@@ -147,7 +159,7 @@ function loadActivities ($scope, $http){
 		loadActivities($scope, $http);
 		dialog.close();
 	};
-	});
+});
 	
 //	return app;
 //}
